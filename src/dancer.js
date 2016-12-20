eb4f8bddfb806e7aecbc21e0226ac58b2c661fe8
Hack Reactor Students <accounts@hackreactor.com>
2016-12-19 18:21:14 -0800

    passes initial tests, passes visual test

--- src/dancer.js
+++ src/dancer.js
@@ -4,7 +4,7 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
 
-  this.step(timeBetweenSteps);
+  this.step.call(this, timeBetweenSteps);
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
